8871284412913da6758d641d04fffa95a4243f58
Eric Chung <ericchung1987@gmail.com>
2020-12-16 01:03:50 +0800

    inital commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const http = require('http');
+const port = process.env.PORT || 3000
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static(__dirname+'/public'));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res){
+    res.render("index.ejs");
+});
+
+app.get("/current-issue", function(req, res){
+    res.render("current-issue.ejs");
+});
+
+app.get("/editorial", function(req, res){
+    res.render("editorial.ejs");
+});
+
+app.get("/instruction", function(req, res){
+    res.render("instruction.ejs");
+});
+
+app.get("/workflow", function(req, res){
+    res.render("workflow.ejs");
+});
+
+app.listen(port,() => {
+    console.log(`Server running at port `+port);
+  });
